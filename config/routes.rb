50bc1c44eb7e5c05c2ef08bdd02becd7a7c44db9
sonali <sonalibhavsar977@gmail.com>
2023-03-30 17:59:26 +0530

    Add confimation pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'sign_up', to: 'users#new'
   post 'sign_up', to: 'users#create'
+
+  resources :confirmations, only: [:create, :new, :edit], params: :confirmation_token
 end
