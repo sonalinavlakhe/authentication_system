ee5e45166b952a4d61b83fc736bb696daad8d1ba
sonali <sonalibhavsar977@gmail.com>
2023-03-30 17:34:45 +0530

    Add methods in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,22 @@ class User < ApplicationRecord
 
   validates :email, format: {with: URI::MailTo::EMAIL_REGEXP} ,presence: true, uniqueness: true
 
+  def confirmed?
+    confirmed_at.present?
+  end
+
+  def unconfirmed?
+    !confirmed?
+  end
+
+  def generate_confirmation_token
+    signed_id expires_in: CONFIRMATION_TOKEN_EXPIRATION, purpose: :confirm_email
+  end
+
+  def confirm!
+    update_column(confirmed_at: Time.current)
+  end
+
   private
 
   def downcase_email
