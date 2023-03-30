3dba46d1b9cee98061190b9991871558a37f9206
sonali <sonalibhavsar977@gmail.com>
2023-03-30 16:27:37 +0530

    1. Added StaticPage controller 2. Added User controller with new and create methods 3. create sign_up pages 4, Added routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 
+  has_secure_password
+  
   before_save :downcase_email
 
   validates :email, format: {with: URI::MailTo::EMAIL_REGEXP} ,presence: true, uniqueness: true
