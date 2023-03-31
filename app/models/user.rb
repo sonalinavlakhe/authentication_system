b54f9170b7488296540331b62390b84bfbfd58d3
sonali <sonalibhavsar977@gmail.com>
2023-03-31 14:54:24 +0530

    Add confirmation pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
 
+  CONFIRMATION_TOKEN_EXPIRATION = 10.minutes
+
   has_secure_password
   
   before_save :downcase_email
 
   validates :email, format: {with: URI::MailTo::EMAIL_REGEXP} ,presence: true, uniqueness: true
+
+  MAILER_FROM_EMAIL = "no-reply@example.com"
 
   def confirmed?
     confirmed_at.present?
@@ -20,6 +24,11 @@ class User < ApplicationRecord
 
   def confirm!
     update_column(confirmed_at: Time.current)
+  end
+
+  def send_confirmation_email!
+    confirmation_token = generate_confirmation_token
+    UserMailer.confirmation(self, confirmation_token).deliver_now
   end
 
   private
