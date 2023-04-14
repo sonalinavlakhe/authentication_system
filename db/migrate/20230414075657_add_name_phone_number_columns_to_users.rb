83468072322a1798b13dc282e8313b2dfc570a24
sonali <sonalibhavsar977@gmail.com>
2023-04-14 13:34:11 +0530

    Add name and phone number columns to user

---
+++ db/migrate/20230414075657_add_name_phone_number_columns_to_users.rb
@@ -0,0 +1,6 @@
+class AddNamePhoneNumberColumnsToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :phone_number, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_29_134526) do
+ActiveRecord::Schema.define(version: 2023_04_14_075657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2023_03_29_134526) do
     t.datetime "updated_at", null: false
     t.datetime "confirmed_at"
     t.string "password_digest", null: false
+    t.string "name"
+    t.integer "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
