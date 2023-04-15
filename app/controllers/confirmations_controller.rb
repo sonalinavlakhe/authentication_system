1cd6b965d9f7a73bcf87523242697376a27da44f
sonali <sonalibhavsar977@gmail.com>
2023-04-15 16:39:38 +0530

    Send confirmation email if user not yet confirmed the email and updated the fined_signed method

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -3,13 +3,15 @@ class ConfirmationsController < ApplicationController
   def create
     @user = User.find_by(email: params[:user][:email].downcase)
     if @user.present? && @user.unconfirmed?
+      @user.send_confirmation_email!
       redirect_to root_path, notice: "Check your email for confirmation instructions."
     else
       redirect_to new_confirmation_path, alert: "We could not find a user with that email or that email has already been confirmed."
+    end
   end
 
   def edit
-    @user = User.find_signed(params[:confirmation_token], purpose: :confirm_email)
+   @user = User.find(Rails.application.message_verifier(:email_confirmation).verify(params[:confirmation_token], purpose: :confirm_email))
     if @user.present?
       @user.confirm!
       redirect_to root_path, notice: "Your account has been confirmed."
