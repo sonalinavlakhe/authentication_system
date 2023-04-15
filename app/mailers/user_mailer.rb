f85efb68a94b452a1a914e9d97d19fae130eaf83
sonali <sonalibhavsar977@gmail.com>
2023-04-15 16:41:14 +0530

    Add User_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,11 @@
 class UserMailer < ApplicationMailer
 
   default from: User::MAILER_FROM_EMAIL
+  layout 'mailer'
   
   def confirmation(user, confirmation_token)
     @user = user
     @confirmation_token = confirmation_token
-
     mail to: @user.email, subject: "Confirmation Instructions"
+  end
 end
