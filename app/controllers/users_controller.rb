3e95c99753d3eee8fb27501ac6af3937bf5a4b4e
sonali <sonalibhavsar977@gmail.com>
2023-04-15 16:40:39 +0530

    Send confirmation email after creating user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      @user.send_confirmation_email!
       redirect_to root_path, notice: "Please check your email for confirmation instructions."
     else
       redirect_to sign_up_path, danger: @user.errors.full_messages.join(", ")
