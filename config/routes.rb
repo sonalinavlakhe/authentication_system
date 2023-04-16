8a9af70cd7ce62f72aeaee7498f44ecd499b011d
sonali <sonalibhavsar977@gmail.com>
2023-04-16 12:34:15 +0530

    Add routes for session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+
   get 'sign_up', to: 'users#new'
   post 'sign_up', to: 'users#create'
+
+  get 'login', to:'sessions#new'
+  post 'login', to:'sessions#create'
+  delete 'logout', to:'sessions#destroy'
 
   resources :confirmations, only: [:create, :new, :edit], param: :confirmation_token
 end
