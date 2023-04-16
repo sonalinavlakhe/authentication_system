e1ed7aa3f166d37098340dfef0fbcf058a61c7d7
sonali <sonalibhavsar977@gmail.com>
2023-04-16 15:02:50 +0530

    Add Password Reset Functionality 1. Update User Model. 2. Update User Mailer. 3. Add layout for mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,6 +6,14 @@ class UserMailer < ApplicationMailer
   def confirmation(user, confirmation_token)
     @user = user
     @confirmation_token = confirmation_token
+    
     mail to: @user.email, subject: "Confirmation Instructions"
+  end
+
+  def password_reset(user, password_reset_token)
+    @user = user
+    @password_reset_token = password_reset_token
+
+    mail to: @user.email, subject: "Password Reset Instructions"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 
   CONFIRMATION_TOKEN_EXPIRATION = 10.minutes
+  PASSWORD_RESET_TOKEN_EXPIRATION = 10.minutes
 
   has_secure_password
   
@@ -19,23 +20,33 @@ class User < ApplicationRecord
   def unconfirmed?
     !confirmed?
   end
+  
+  def confirm!
+    update_column(:confirmed_at, Time.current)
+  end
+
+  def generate_password_reset_token
+    verifier = Rails.application.message_verifier(Rails.application.secret_key_base)
+    signed_id = verifier.generate({id: self.id, purpose: :reset_password}, expires_in: PASSWORD_RESET_TOKEN_EXPIRATION)
+    signed_id
+  end
+
+  def send_password_reset_email!
+    password_reset_token = generate_password_reset_token
+    UserMailer.password_reset(self, password_reset_token).deliver_now
+  end
 
   def generate_confirmation_token
     verifier = Rails.application.message_verifier(Rails.application.secret_key_base)
     signed_id = verifier.generate({id: self.id, purpose: :confirm_email}, expires_in: CONFIRMATION_TOKEN_EXPIRATION)
-
     signed_id
-  end
-
-  def confirm!
-    update_column(:confirmed_at, Time.current)
   end
 
   def send_confirmation_email!
     confirmation_token = generate_confirmation_token
     UserMailer.confirmation(self, confirmation_token).deliver_now
   end
-
+  
   private
 
   def downcase_email
