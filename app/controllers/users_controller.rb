6e936c90dc99c4162f425d64028563a757414ad2
sonali <sonalibhavsar977@gmail.com>
2023-04-19 17:37:34 +0530

    Change notice message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       @user.send_confirmation_email!
-      redirect_to root_path, notice: "Please check your email for confirmation instructions."
+      redirect_to root_path, notice: "Please check your email for confirmation instructions also verification code sent on the registered mobile #{@user.phone_number}"
     else
       redirect_to sign_up_path, danger: @user.errors.full_messages.join(", ")
     end
